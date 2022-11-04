b1dbcd167b6d6284c260f47c4d71bbbe91cf0895
Anderboi <Upolovnew@gmail.com>
2022-11-04 15:51:06 +0300

    feat: 4 task is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const { constants } = require("buffer");
+const {
+  readdir,
+  createReadStream,
+  writeFile,
+  createWriteStream,
+} = require("fs");
+const { mkdir, copyFile } = require("fs/promises");
+const path = require("path");
 
+mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+const baseFolder = path.join(__dirname, "files");
+
+readdir(baseFolder, (err, files) => {
+  err && console.log(err);
+
+  for (const file of files) {
+    copyFile(
+      path.join(__dirname, "files", file),
+      path.join(__dirname, "files-copy", file),
+      constants.COPYFILE_FICLONE
+    );
+  }
+});
