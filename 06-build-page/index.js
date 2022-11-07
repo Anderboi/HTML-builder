5d4a81cbaabdc813922605b6cb3a06e354193d2b
Anderboi <Upolovnew@gmail.com>
2022-11-07 12:44:07 +0300

    fix: 06 task is done

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const { constants } = require("buffer");
 
 const baseHTML = path.join(__dirname, "template.html");
 
@@ -30,3 +31,56 @@ fs.promises.readFile(baseHTML, "utf-8").then((data) => {
     }
   });
 });
+
+const output = fs.createWriteStream(
+  path.join(__dirname, "project-dist", "style.css")
+);
+fs.readdir(
+  path.join(__dirname, "styles"),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      //*check if *file* is not a folder and is css-file
+      if (!file.isDirectory() && path.parse(file.name).ext === ".css") {
+        const readableStream = fs.createReadStream(
+          path.join(__dirname, "styles", file.name)
+        );
+        let data = "";
+        readableStream.on("data", (chunk) => {
+          output.write((data += `${chunk.toString()}\n`));
+        });
+      }
+    }
+  }
+);
+
+function copyDir(folderPath, distPath) {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      if (path.parse(file).ext) {
+        fs.copyFile(
+          path.join(folderPath, file),
+          path.join(distPath, file),
+          constants.COPYFILE_FICLONE,
+          (err) => {
+            if (err) throw err.message;
+          }
+        );
+      } else {
+        fs.promises
+          .mkdir(path.join(distPath, file), { recursive: true })
+          .then(() => {
+            copyDir(path.join(folderPath, file), path.join(distPath, file));
+          });
+      }
+    }
+  });
+}
+copyDir(
+  path.join(__dirname, "assets"),
+  path.join(__dirname, "project-dist", "assets")
+);
